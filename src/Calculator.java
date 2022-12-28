896781652810b268e4e03c99809b47cf6800a806
howback <gricyk1996@gmail.com>
2022-12-28 15:20:10 +0200

    first download

---
+++ src/Calculator.java
@@ -0,0 +1,87 @@
+import java.util.Stack;
+
+public class Calculator {
+    public double decide(String verifiedString) {
+        String rpn = expressionToRPN(verifiedString);
+        return rpnToAnswer(rpn);
+    }
+    public int amountOfNumbers(String expression) {
+        int amount = 0;
+        for (int i = 1; i < expression.length(); i++) {
+            char token = expression.charAt(i);
+            char past = expression.charAt(i - 1);
+            if (getPriority(past) == 0 && getPriority(token) != 0) amount += 1;
+        }
+        if (getPriority(expression.charAt(expression.length() - 1)) == 0) amount += 1;
+        return amount;
+    }
+    private String expressionToRPN(String expr) {
+        String current = "";
+        Stack<Character> stack = new Stack<>();
+        int priority;
+        for (int i = 0; i < expr.length(); i++) {
+            priority = getPriority(expr.charAt(i));
+            if (priority == 0) {
+                current += expr.charAt(i);
+            }
+            if (priority == 1) {
+                stack.push(expr.charAt(i));
+            }
+            if (priority > 1) {
+                current += " ";
+                while (!stack.empty()) {
+                    if (getPriority(stack.peek()) >= priority) current += stack.pop();
+                    else break;
+                }
+                stack.push(expr.charAt(i));
+            }
+            if (priority == -1) {
+                current += " ";
+                while (getPriority(stack.peek()) != 1) current += stack.pop();
+                stack.pop();
+            }
+        }
+        while (!stack.empty()) current += stack.pop();
+        return current;
+    }
+    private double rpnToAnswer(String rpn) {
+        String operand = "";
+        Stack<Double> stack = new Stack<>();
+
+        for (int i = 0; i < rpn.length(); i++) {
+            if (rpn.charAt(i) == ' ') continue;
+            if (getPriority(rpn.charAt(i)) == 0) {
+                while (rpn.charAt(i) != ' ' && getPriority(rpn.charAt(i)) == 0) {
+                    operand += rpn.charAt(i++);
+                    if (i == rpn.length()) break;
+                }
+                stack.push(Double.parseDouble(operand));
+                operand = "";
+            }
+            if (getPriority(rpn.charAt(i)) > 1) {
+                double last = stack.pop(), penultimate = stack.pop();
+                if (rpn.charAt(i) == '+') stack.push(penultimate + last);
+                if (rpn.charAt(i) == '-') stack.push(penultimate - last);
+                if (rpn.charAt(i) == '*') stack.push(penultimate * last);
+                if (rpn.charAt(i) == '/') {
+                    if (last == 0) {
+                        new Verifier().throwsAnError("byZero");
+                    } else stack.push(penultimate / last);
+                }
+            }
+        }
+        return stack.pop();
+}
+    //set priorities for characters (*,/)=3 (+,-)=2  '('=1  ')'=-1  (0-9)=0
+     static int getPriority(char token) {
+        if (token == '*' || token == '/') {
+            return 3;
+        } else if (token == '+' || token == '-') {
+            return 2;
+        } else if (token == '(') {
+            return 1;
+        } else if (token == ')') {
+            return -1;
+        } else return 0;
+    }
+}
---
+++ src/Verifier.java
@@ -0,0 +1,102 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Verifier {
+
+    public void verification(String expression){
+        Pattern pattern = Pattern.compile("^((\\d*\\.?\\d*)([+-/*]))*(\\d*\\.?\\d*)$");
+        Matcher matcher = pattern.matcher(expression);
+        if(matcher.matches()) {
+            String verifiedString = examination(expression);
+            checkBrackets(expression);
+            if (!examination(expression).equals(expression)) {
+                System.out.println("Simplified expression = " + examination(expression));
+            }
+            System.out.println("Answer = " + new Calculator().decide(verifiedString));
+            System.out.println("Amount of numbers in expression = " + new Calculator().amountOfNumbers(expression));
+        } else System.err.println("Input Error. Valid characters: 0-9 and the following characters: + - / * ( ) .");
+    }
+    void throwsAnError(String error){
+        if(error.equals("sings")){
+            System.err.println("Input Error arithmetic sings");
+            System.exit(1);
+        }
+        if(error.equals("brackets")){
+            System.err.println("incorrect placement of arithmetic brackets!");
+            System.exit(1);
+        }
+        if(error.equals("start")){
+            System.err.println("Input Error, the expression must start with a digit, a minus sign, or an opening arithmetic bracket");
+            System.exit(1);
+        }
+        if(error.equals("byZero")){
+            System.err.println("divide by zero is not allowed!");
+            System.exit(1);
+        }
+
+    }
+
+    private void checkBrackets(String expression) {
+        int open = 0, close = 0;
+        for (int i = 0; i < expression.length(); i++) {
+            char token = expression.charAt(i);
+
+            if (i == 0) {
+                if (token == '*' || token == '/') {
+                    throwsAnError("start");
+                }
+            }
+
+            if (token == '*' || token == '/') {
+                if (expression.charAt(i - 1) == '*' || expression.charAt(i - 1) == '/') {
+                    throwsAnError("sings");
+                } else if (expression.charAt(i - 1) == '+' || expression.charAt(i - 1) == '-') {
+                    throwsAnError("sings");
+                }
+            } else if (token == '+' || token == '-') {
+                if (expression.charAt(i - 1) == '+' || expression.charAt(i - 1) == '-') {
+                    throwsAnError("sings");
+                }
+            } else if (token == '(') {
+                open += 1;
+            } else if (token == ')') {
+                close += 1;
+                if (close > open) {
+                    throwsAnError("brackets");
+                }
+                if (open != close) {
+                    throwsAnError("brackets");
+                }
+            }
+        }
+    }
+
+    private String examination(String expression) {
+        String transformedExpression = "";
+        List<Character> list = new ArrayList<>();
+        for (int i = 0; i < expression.length(); i++) {
+            list.add(expression.charAt(i));
+        }
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == '-') {
+                if (i == 0) list.add(0, '0');
+                else if (list.get(i - 1) == '*' || list.get(i - 1) == '/') {
+                    list.add(i, '0');
+                    list.add(i, '(');
+                    int s = 3 + i;
+                    while (Calculator.getPriority(list.get(s)) == 0) {
+                        s = s + 1;
+                        if (s == list.size()) list.add(')');
+                    }
+                    if (s != list.size() - 1) list.add(s, ')');
+                }
+            }
+        }
+        for (int i = 0; i < list.size(); i++) {
+            transformedExpression += list.get(i);
+        }
+        return transformedExpression;
+    }
+}
